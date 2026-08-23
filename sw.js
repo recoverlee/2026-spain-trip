bcb4e5eb27ff58128b78287f16c470657501ff77
recoverlee <jhlee8379@gmail.com>
2026-08-23 14:31:07 +0900

    feat: add PWA installation support

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "spain-trip-pwa-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request).then(response => {
+        const responseCopy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseCopy));
+        return response;
+      });
+    })
+  );
+});
